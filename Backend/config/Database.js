11caec8697758473603425507bbf631084690cca
Kushang Tanawala <kushangtanawala24@gmail.com>
2024-12-19 10:42:34 +0530

    Note Application Using MERN Stack With CEUD And Specific User Data

--- Backend/config/Database.js
+++ Backend/config/Database.js
@@ -2,7 +2,7 @@ const mongoose = require('mongoose');
 
 const db = async () => {
     try {
-        await mongoose.connect("mongodb://localhost:27017/iNoteBook");
+        await mongoose.connect("mongodb+srv://kushangtanawala24:Admin1234@cluster0.skaf2.mongodb.net/Note-app");
         console.log("Database connected");
     } catch (error) {
         console.error("Database connection error:", error);
